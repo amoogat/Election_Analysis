009cfcb94a31c4ab68eceb8d312b5c0444a98dd2
Aarian Moogat <amoogat@gmail.com>
2020-03-29 15:23:57 -0700

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -28,6 +28,7 @@ with open(file_to_load) as election_data:
 		# gets a complete list of canditates that recieved votes
 		candidate = row [2]
 		
+		#adds canddiate names to candidate list
 		if candidate not in candidate_options:
 			candidate_options.append(candidate)
 			# begins tracking candidates vote count
@@ -35,31 +36,45 @@ with open(file_to_load) as election_data:
 
 		# adds vote to the value of the key which is candidates name
 		candidate_votes[candidate] += 1
-
-	 # count and percentage of votes won for each candidate 
-	for candidate in candidate_options: 
-		votes = candidate_votes[candidate]
-		vote_percentage = votes/ total_votes * 100
+	with open(file_to_save, "w") as txt_file:
+		# Print the final vote count to the terminal.
+		election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+		print(election_results, end="")
+    	# Save the final vote count to the text file.
+		txt_file.write(election_results)
 		
-		#prints candidate name, vote count and percentage
-		print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+		# count and percentage of votes won for each candidate 
+		for candidate in candidate_options: 
+			votes = candidate_votes[candidate]
+			vote_percentage = votes/ total_votes * 100
+			# gets candidate name, vote count and percentage
+			candidate_results = (f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+			print(candidate_results)
+			#saves candidate results to txt file
+			txt_file.write(candidate_results)
 
-		# this checks if the votecount, % is higher than previous candidate
-		if ((votes > winning_count) and (vote_percentage > winning_percentage)):
+			# checks if votes and % is higher than previous candidate
+			if ((votes > winning_count) and (vote_percentage > winning_percentage)):
 
-			# if above is true, then this candidate WINS and had this count, %
-			winning_count = votes
-			winning_percentage = vote_percentage
-			winning_candidate = candidate
+				# if above is true, then this candidate WINS and had this count, %
+				winning_count = votes
+				winning_percentage = vote_percentage
+				winning_candidate = candidate
+			
+		winning_candidate_summary = (
+			f"-------------------------\n"
+				f"Winner: {winning_candidate}\n"
+				f"Winning Vote Count: {winning_count:,}\n"
+				f"Winning Percentage: {winning_percentage:.1f}%\n"
+				f"-------------------------\n")
+
+		print(winning_candidate_summary)
+		#save winning candidatest summary to txt file
+		txt_file.write(winning_candidate_summary)
 		
-	winning_candidate_summary = (
-		f"-------------------------\n"
-    		f"Winner: {winning_candidate}\n"
-    		f"Winning Vote Count: {winning_count:,}\n"
-    		f"Winning Percentage: {winning_percentage:.1f}%\n"
-    		f"-------------------------\n")
-	print(winning_candidate_summary)
-
-	
 
 
